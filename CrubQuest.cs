f1ed7718d13b857e1dcbdfeaff653017874c56a2
ZestyZest <asanft@sanft.com>
2026-03-04 23:09:38 -0600

    Second commit
    dont make me describe things

--- CrubQuest.cs
+++ CrubQuest.cs
@@ -3,7 +3,12 @@ class CrubQuest
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, Bitch!");
-        Console.WriteLine(NumberTime(6,7));        
+        Console.WriteLine(NumberTime(6, 7));
+        var dog = new Dog(5)
+        {
+            Name = "BleepBoop"
+        };
+        Console.WriteLine($"Dog's name: {dog.Name}, Age: {dog.Age}");
     }
 
     static int NumberTime(int num1, int num2)
---
+++ Dog.cs
@@ -0,0 +1,12 @@
+
+public class Dog
+{
+    public string Name { get; set; } = "Bilbo";
+    public int Age { get; set; }
+
+    public Dog(int age)
+    {
+
+        Age = age;
+    }
+}
