23bf8c0eafaa6d9809484208a3d8b686caa507af
ZestyZest <asanft@sanft.com>
2026-03-04 22:09:01 -0600

    Initialization
    I did the thign

---
+++ CrubQuest.cs
@@ -0,0 +1,13 @@
+class CrubQuest
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Bitch!");
+        Console.WriteLine(NumberTime(6,7));        
+    }
+
+    static int NumberTime(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
