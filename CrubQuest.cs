d441148e578bccf01b9741afe00cc97925e0459e
Nick Sanft <nsanft@sanft.com>
2026-03-04 23:27:47 -0600

    Update .gitignore, cleanup

--- CrubQuest.cs
+++ CrubQuest.cs
@@ -1,24 +1,10 @@
-
-using System;
-
 namespace Crub
 {
     class CrubQuest
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Bitch!");
-            Console.WriteLine(NumberTime(6, 7));
-            var dog = new Dog(5)
-            {
-                Name = "BleepBoop"
-            };
-            Console.WriteLine($"Dog's name: {dog.Name}, Age: {dog.Age}");
-        }
-
-        static int NumberTime(int num1, int num2)
-        {
-            return num1 + num2;
+            Console.WriteLine("Hello, Friend!");
         }
     }
 }
